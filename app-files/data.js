956b07c017ca7880e2940f10277d659cffe5df19
RadaRish <107124341+RadaRish@users.noreply.github.com>
2022-06-11 12:06:50 +0300

    Загрузили всё
    Загружены все папки и файлы

---
+++ app-files/data.js
@@ -0,0 +1,389 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "Вход",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.021495137239337936,
+        "pitch": -0.008456976093325963,
+        "fov": 1.5495592092183668
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.0832320319872597,
+          "pitch": -0.01948362106483259,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-",
+      "name": "Ресепшен",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -3.1103630113634395,
+        "pitch": 0.08018189899807027,
+        "fov": 1.5495592092183668
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0099565423278225,
+          "pitch": 0.14819598508014664,
+          "rotation": 0,
+          "target": "2--"
+        },
+        {
+          "yaw": -3.110031513263568,
+          "pitch": 0.07985040089819861,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2--",
+      "name": "Второй этаж",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.5495592092183668
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8052974495690677,
+          "pitch": 0.15194990876285175,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": -2.998664223384928,
+          "pitch": 0.3176461364243224,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": 0.4114758968339771,
+          "pitch": 0.34690131007062064,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-",
+      "name": "Холл",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 2.7188042758675124,
+        "pitch": 0.15418070688744123,
+        "fov": 1.5495592092183668
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.4370297897257611,
+          "pitch": 0.4973268445381738,
+          "rotation": 0,
+          "target": "2--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-",
+      "name": "Коридор",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.257246354967851,
+        "pitch": 0.029799215561887138,
+        "fov": 1.5495592092183668
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.668492581463023,
+          "pitch": 0.2723004117422363,
+          "rotation": 0,
+          "target": "8--"
+        },
+        {
+          "yaw": -0.4600826212342355,
+          "pitch": 0.23756493781270294,
+          "rotation": 0,
+          "target": "7--"
+        },
+        {
+          "yaw": -2.5361460683011057,
+          "pitch": 0.1504660373727127,
+          "rotation": 0,
+          "target": "6--"
+        },
+        {
+          "yaw": -2.9068744945567815,
+          "pitch": 0.08608390349986372,
+          "rotation": 0,
+          "target": "5--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5--",
+      "name": "Комфорт Твин",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 2.932926078143545,
+        "pitch": 0.032753293519462545,
+        "fov": 1.5495592092183668
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7844397809235328,
+          "pitch": 0.14985353566700432,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6--",
+      "name": "Комфорт Дабл",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 1.4729061209221648,
+        "pitch": 0.005959843112389507,
+        "fov": 1.5495592092183668
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.339283688172751,
+          "pitch": 0.14550571669663626,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7--",
+      "name": "Комфорт Сингл",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.9584445677447953,
+        "pitch": 0.07738121555415489,
+        "fov": 1.5495592092183668
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.479372756614719,
+          "pitch": 0.16644322870247663,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8--",
+      "name": "Джуниор Сьюит",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -2.2399563698082297,
+        "pitch": 0.10422573952619629,
+        "fov": 1.5495592092183668
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.695132633592447,
+          "pitch": 0.25410484673821365,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Old City Hotel",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
